0fed3be6afd1227c852adce5fdf7b3cd9a474c97
margodo <margaritadoand@gmail.com>
2024-12-04 02:39:50 +0100

    Slight change to main logic

--- bookstore/main.py
+++ bookstore/main.py
@@ -13,21 +13,21 @@ bookstore_db.create_table(table_name='bookstore', columns={
         "amount": "INTEGER",
     })
 
-bookstore_db.add_book(
-    table_name='bookstore',
-    data = [
-        (None, 'Anna Karenina', 'Lev Tolstoj', '2022-10-21','Novel', 2100.50, 7),
-        (None, 'Crime and Punishment', 'Fyodor Dostoevsky', '2018-06-15','Novel', 1980.00, 10),
-        (None, 'Romeo and Juliette', 'William Shakespeare', '2020-02-28','Tragedy', 2050.40, 4),
-        (None, 'The Old Man and the Sea', 'Ernest Hemingway', '2016-04-14','Novel', 2430.70, 2),
-        (None, 'Lolita', 'Vladimir Nabokov', '2021-01-10','Novel', 1990.00, 5),
-        (None, 'The Autobiography of Benjamin Franklin', 'Benjamin Franklin', '2008-08-22','Autobiography', 2540.10, 4),
-        (None, 'Woe from Wit', 'Alexander Griboedov', '2015-12-14','Comedy', 1750.80, 9),
-        (None, 'Poems', 'Sergey Yesenin', '2017-03-11','Poem', 1670.50, 10),
-        (None, 'The Count of Monte Cristo', 'Alexandre Dumas', '2011-11-11','Novel', 4300.40, 4),
-        (None, 'All Quiet on the Western Front', 'Erich Maria Remarque', '2012-06-27','Autobiographical novel', 2230.10, 8),
-        (None, 'Hamlet', 'William Shakespeare', '2014-09-10','Tragedy', 1800.20, 1)
-    ])
+# bookstore_db.add_book(
+#     table_name='bookstore',
+#     data = [
+#         (None, 'Anna Karenina', 'Lev Tolstoj', '2022-10-21','Novel', 2100.50, 7),
+#         (None, 'Crime and Punishment', 'Fyodor Dostoevsky', '2018-06-15','Novel', 1980.00, 10),
+#         (None, 'Romeo and Juliette', 'William Shakespeare', '2020-02-28','Tragedy', 2050.40, 4),
+#         (None, 'The Old Man and the Sea', 'Ernest Hemingway', '2016-04-14','Novel', 2430.70, 2),
+#         (None, 'Lolita', 'Vladimir Nabokov', '2021-01-10','Novel', 1990.00, 5),
+#         (None, 'The Autobiography of Benjamin Franklin', 'Benjamin Franklin', '2008-08-22','Autobiography', 2540.10, 4),
+#         (None, 'Woe from Wit', 'Alexander Griboedov', '2015-12-14','Comedy', 1750.80, 9),
+#         (None, 'Poems', 'Sergey Yesenin', '2017-03-11','Poem', 1670.50, 10),
+#         (None, 'The Count of Monte Cristo', 'Alexandre Dumas', '2011-11-11','Novel', 4300.40, 4),
+#         (None, 'All Quiet on the Western Front', 'Erich Maria Remarque', '2012-06-27','Autobiographical novel', 2230.10, 8),
+#         (None, 'Hamlet', 'William Shakespeare', '2014-09-10','Tragedy', 1800.20, 1)
+#     ])
 
 all_books = bookstore_db.all_books(table_name='bookstore')
 
@@ -55,11 +55,9 @@ def home():
                 updates=f'amount = {amount-1}',
                 condition=f'id={id}'
             )
-            purchased_book = bookstore_db.search_book(table_name='bookstore', condition=f'id={id}')
-            if purchased_book[0][-1] == 0:
-                bookstore_db.delete(table_name='bookstore', condition=f'id={id}')
             message = "Thank you for purchase"
         else:
+            bookstore_db.delete(table_name='bookstore', condition=f'id={id}')
             message = "Sorry, this book is out of stock."
         return render_template('index.html', message=message)
     all_books = bookstore_db.all_books(table_name='bookstore')
