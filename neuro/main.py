91905292c0c9bc7a464145bd1f58c9e0e7b28e28
margodo <margaritadoand@gmail.com>
2025-01-13 14:43:43 +0100

    Adding new data and graph

--- neuro/main.py
+++ neuro/main.py
@@ -2,6 +2,24 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+from random import randint
+import matplotlib.pyplot as plt
+
+data = []
+prices = []
+for i in range(0,100):
+    square = randint(20,160)
+    if square < 50:
+        rooms = randint(1,2)
+        price = randint(100,150)
+    elif square < 100:
+        rooms = randint(3,5)
+        price = randint(180,500)
+    else:
+        rooms = randint(5,8)
+        price = randint(550,1000)
+    prices.append(price)
+    data.append([square, rooms])
 
 class  NeuralNetworkExample:
 
@@ -11,25 +29,41 @@ class  NeuralNetworkExample:
         self.y_train = None
 
     def prepare_data(self):
-        self.x_train = np.array([[30,1],[50,2],[70,3],[90,4],[120,5]], dtype=np.float32)
-        self.y_train = np.array([100,150,200,250,300], dtype=np.float32)
+        self.x_train = np.array(data, dtype=np.float32)
+        self.y_train = np.array(prices, dtype=np.float32)
     
     def build_model(self):
         self.model = Sequential([
             Dense(10, input_dim=2, activation ='relu'),
             Dense(1)
         ])
-        self.model.complie(optimizer='adam', loss='mean_squared_error')
+        self.model.compile(optimizer='adam', loss='mean_squared_error')
     
     def train_model(self):
-        self.model.fit(self.x_trian, self.y_train, epochs=100, verbose=1)
+        self.model.fit(self.x_train, self.y_train, epochs=100, verbose=1)
 
     def test_model(self):
         x_test = np.array([[40,1],[80,3],[100,4]], dtype=np.float32)
         prediction = self.model.predict(x_test)
         print('Result:')
         for i,pred in enumerate(prediction):
-            print(f'Data: {x_test[i]} - prection: {pred[0]:.2f} US dollars.')
+            print(f'Data: {x_test[i]} - prediction: {pred[0]:.2f} US dollars.')
+    
+    def plot_predictions(self):
+        predicted_prices = self.model.predict(self.x_train)
+
+        plt.figure(figsize=(10,6))
+        
+        plt.scatter([x[0] for x in self.x_train], self.y_train, color='blue', label='Actual Prices', alpha=0.5)
+
+        plt.scatter([x[0] for x in self.x_train], predicted_prices, color='red', label='Predicted Prices', alpha=0.5)
+
+        plt.title("Actual vs Predicted Apartment Prices")
+        plt.xlabel("Apartment Size (m²)")
+        plt.ylabel("Price (US Dollars)")
+        plt.legend()
+        plt.show()
+        
 
 if __name__ == "__main__":
     nn_example = NeuralNetworkExample()
@@ -37,3 +71,4 @@ if __name__ == "__main__":
     nn_example.build_model()
     nn_example.train_model()
     nn_example.test_model()
+    nn_example.plot_predictions()
